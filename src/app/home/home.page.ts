190a2d622be458c44bf0575255813719399cf4ab
Watchara Ketkaew <wk996110@gmail.com>
2024-06-26 14:00:46 +0700

    ใส่อะไรก็ได้

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,28 @@ export class HomePage {
 
   constructor() {}
 
+  weight ? : number;
+  hight ? : number;
+  result ? : number;
+  result_text? : string="";
+
+  oncal(){
+    if(this.hight == undefined || this.weight == undefined){
+        alert("กรุณากรอกข้อมูลให้ครบ");
+        return;
+    }
+    var hi = this.hight / 100;
+    this.result = this.weight / (hi*hi);
+    if(this.result < 18.5){
+      this.result_text = "อยู่ในเกณฑ์น้ำหนักน้อยหรือผอม";
+    }else if(this.result >= 18.5 && this.result <= 22.90){
+      this.result_text = "อยู่ในเกณฑ์ปกติ";
+    }else if(this.result >= 23 && this.result <= 24.90){
+      this.result_text = "น้ำหนักเกิน";
+    }else if(this.result >= 25 && this.result <= 29.90){
+      this.result_text = "โรคอ้วนระยะที่ 1";
+    }else{
+      this.result_text = "โรคอ้วนระยะที่ 2";
+    }
+  }
 }
